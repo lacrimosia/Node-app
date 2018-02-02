7dd5e7c0bf8837504d93a02c95add3ff5578e2a3
Shatilla Prayer <prayers@unlv.nevada.edu>
2018-02-01 16:56:11 -0800

    added download page

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var app = express();
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./views/index.html', 'utf8');
-var options = { format: 'Letter' };
+var options = {
+    format: 'Letter',
+    border: {
+        "top": "1in", // default is 0, units: mm, cm, in, px
+        "right": "1in",
+        "bottom": "1in",
+        "left": "1in"
+    }
+};
+var location = require('location-href');
+location();
 
 
 app.use(express.static(path.join(__dirname, 'views')));
@@ -13,10 +23,11 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/index.html
 app.get('/sub', (req, res) => res.sendFile(path.join(__dirname + '/views/sub.html')));
 
 app.get('/newfile', (req, res) => {
-    pdf.create(html, options).toFile('./file.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
- });
+    pdf.create(html, options).toFile('./file.pdf',(err, res) => {
+        if (err) return console.log(err);
+        console.log('File saved.', res);
+    }); 
+    res.sendFile(path.join(__dirname + '/views/download.html'));  
 });
 
-app.listen(5000, () => console.log("app running"));
+//app.listen(5000, () => console.log("app running"));
