faa97362bf33d6c2cb6c630c6fceead1202e9772
Shatilla Prayer <prayers@unlv.nevada.edu>
2018-02-15 16:44:02 -0800

    played

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ var location = require('location-href');
 location();
 
 var _ = require("lodash");
-
+var http = require("http");
+const dns = require('dns');
+const url = require('url');
+const os = require('os');
+var nodemailer = require('nodemailer'); 
 
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -34,14 +38,17 @@ app.get('/newfile', (req, res) => {
 
 var a = [1,2,3,"copper",4,5];
 var changes = _.indexOf(a, "copper");
-console.log(setProfile({name: "Sia Termine", gender: "F", age: 21}));
+//console.log(setProfile({name: "Sia Termine", gender: "F", age: 21}));
 function setProfile(o){
     o.name = o.name || null;
     o.age = o.age >= 21 || null;
     o.gender = o.gender || null;
     return o;
 }
+var link = location.href;
+//console.log('path stuff', path.basename('seela@gmail.com'));
 //console.log(changes);
+console.log("node stuff", os.type());
 
 // download nodemon for node server
 var port = 5000;
