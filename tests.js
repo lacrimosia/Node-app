7777a1075015b1a8c217f1f9727b65f1521bd0f6
Shatilla Prayer <prayers@unlv.nevada.edu>
2018-02-23 15:43:51 -0800

    Added practice files and more edits

--- tests.js
+++ tests.js
@@ -1,7 +1,9 @@
 // writing asynchronous code with callbacks
 
+// testing function 
+// the function we would use to run regular stuff
 var a = function(e, callback){
-	var limit = 5;
+	var limit = 3;
 	if(e > limit){
 		callback(null, e);
 	}else{
@@ -9,6 +11,9 @@ var a = function(e, callback){
 	}
 }
 
+// callback function
+// first parameter on a callback is the error message
+// the second parameter is the other custom parameter if there is no error
 var getAnswers = function(err, results){
 	if(err){
 		console.log('error message '+err.message);
@@ -17,6 +22,7 @@ var getAnswers = function(err, results){
 	}
 }
 
+// calling the a function to print out all numbers from -1 to 9
 for(var i=-1; i<10; i++){
    a(i, getAnswers);
 }
